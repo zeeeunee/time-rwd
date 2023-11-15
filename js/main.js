cc39529f2fd1dfab3bd856dddd06fac820f67480
zeeeunee <wldmstv@gmail.com>
2023-11-15 13:19:09 +0900

    자주 바뀌는 값을 전역변수로 빼서 함수에 전달

--- js/main.js
+++ js/main.js
@@ -20,8 +20,17 @@ setInterval(() => {
 */
 //------------------------------------------------
 
+//해당 값이 아래 함수에서 호출되도록 처리 (전역변수)
+const data = [
+	{ cond: new Date().getHours() >= 5 && new Date().getHours() < 12, name: 'morning' },
+	{ cond: new Date().getHours() >= 12 && new Date().getHours() < 18, name: 'afternoon' },
+	{ cond: new Date().getHours() >= 18 && new Date().getHours() < 21, name: 'evening' },
+	{ cond: new Date().getHours() >= 21 || new Date().getHours() < 5, name: 'night' },
+];
+
 setInterval(() => {
-	changeTheme();
+	//data전역변수를 인수로 받아서 호출처리
+	changeTheme(data);
 	em.innerText = new Date().getHours() < 12 ? 'am' : 'pm';
 	//getTime함수가 [시간,분,초]반환
 	//반환된 배열값을 그대로 반복돌면서 setTime함수에 인수로 전달
@@ -47,17 +56,12 @@ function setTime(num, index) {
 }
 
 //시간에 테마 변경 함수
-function changeTheme() {
+function changeTheme(info) {
+	//전역 data를 바로 활용하는 것이 아닌 info라는 파라미터를 통해서 전달받도록 처리
 	const hr = new Date().getHours();
 	main.className = ''; //클래스 비우기
 
-	const data = [
-		{ cond: hr >= 5 && hr < 12, name: 'morning' },
-		{ cond: hr >= 12 && hr < 18, name: 'afternoon' },
-		{ cond: hr >= 18 && hr < 21, name: 'evening' },
-		{ cond: hr >= 21 || hr < 5, name: 'night' },
-	];
-	data.forEach((el) => {
+	info.forEach((el) => {
 		if (el.cond) main.classList.add(el.name);
 	});
 
